ce28a44fe9b1db9336463b482dd6ede7e12a03a9
Diomande <38303906+francisbeboy@users.noreply.github.com>
2021-06-05 12:14:14 +0100

    Update min.py

--- min.py
+++ min.py
@@ -4,19 +4,18 @@ print("2:Ajouter")
 print("3:Afficher")
 print("4:vider")
 print("5:quiter")
-choix=int(input("votre choix :  "))
+choix=int(input("votre choix : "))
 while (choix>=0 and choix <=5): 
-     choix= int(input("entrez votre choix :"))    
+     choix= int(input("Quel choix voulez-vous fait parmis ces 5 options :"))    
      if choix==1:
           element= input("Donnez l'element à ajouter :")
           liste.append(element)
           print("L'element à bien été ajouter à la liste")
-          print(liste)
      elif (choix==2):
           element=input("donner le nom de l'element ")
           if element in liste:
                liste.remove(element)
-               print("l'élément bien ajouter à la liste!")
+               print("l'élément a  bien été retirer de la liste!")
      elif (choix==3):
           print(liste)
      elif (choix==4):
