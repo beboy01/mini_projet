e536abcc022d39cb225a20c0ba8e69b393991684
Diomande <38303906+francisbeboy@users.noreply.github.com>
2021-06-06 12:20:14 +0100

    Update min.py

--- min.py
+++ min.py
@@ -1,25 +1,37 @@
 liste=[]
-print("1:Ajouter")
-print("2:Ajouter")
-print("3:Afficher")
-print("4:vider")
-print("5:quiter")
-choix=int(input("votre choix : "))
-while (choix>=0 and choix <=5): 
-     choix= int(input("Quel choix voulez-vous fait parmis ces 5 options :"))    
-     if choix==1:
-          element= input("Donnez l'element à ajouter :")
-          liste.append(element)
-          print("L'element à bien été ajouter à la liste")
-     elif (choix==2):
-          element=input("donner le nom de l'element ")
-          if element in liste:
-               liste.remove(element)
-               print("l'élément a  bien été retirer de la liste!")
-     elif (choix==3):
-          print(liste)
-     elif (choix==4):
-          liste.clear()
-     elif (choix==5):
-          exit()
+choix=1
+cont=0
+for choix in range(1,5):
+
+     while (choix>=0 and choix <=5): 
+          try:
+               print("-------------------------------------------------\nChoissez parmie ces 5 options suivantes :\n1:Ajouter un element à la liste\n2:Retirer un element de la liste\n3:Afficher la liste\n4:vider la liste\n5:quiter le programme")
+               choix=int(input("   votre choix :"))
+          except:
+               print("vous devez entrez u nombre") 
+          if choix==1:
+               element= input("Donnez l'element à ajouter :")
+               liste.append(element)
+               print("L'element à bien été ajouter à la liste")
+          elif (choix==2):
+               element=input("donner le nom de l'element ")
+               if element in liste:
+                    liste.remove(element)
+                    cont-=1
+                    print("l'élément a  bien été retirer de la liste!")
+          elif (choix==3):
+               print("voici les informations que vous avez renseignez")
+               for i in liste:
+                    cont+=1
+                    print(f"{cont}.  {i}")
+               if not (i in liste):
+                    print("votre liste ne contient aucun element")
+                    cont=0
+          elif (choix==4):
+               liste.clear()
+               print("vous avez vider la liste")
+          elif (choix==5):
+               print("vous avez quitez le programme mercie")
+               exit()
+               
 
