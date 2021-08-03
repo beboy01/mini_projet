79f962b1297d99c65e0577a0d08b9fa145855699
beboy01 <74278314+beboy01@users.noreply.github.com>
2021-08-03 02:41:49 +0100

    Update min.py

--- min.py
+++ min.py
@@ -1,37 +1,55 @@
 liste=[]
-choix=1
-cont=0
-for choix in range(1,5):
 
-     while (choix>=0 and choix <=5): 
+print("-------------------------------------------------\nChoissez parmie ces 5 options suivantes :\n1:Ajouter un element à la liste\n2:Retirer un element de la liste\n3:Afficher la liste\n4:vider la liste\n5:quiter le programme")
+choix=input("   votre choix :")
+
+ranger= ["1","2","3","4","5"]
+try:
+     choix =int(choix)
+except errmsg("Vous devez entrer un nombre compris entre 1 et 5"):
+     print(errmsg())
+                
+for choix in ranger:
+     suivant = input("Voulez-vous ajouter autre element (oui/non)?: ")
+     while suivant=="oui":
+          print("-------------------------------------------------\nChoissez parmie ces 5 options suivantes :\n1:Ajouter un element à la liste\n2:Retirer un element de la liste\n3:Afficher la liste\n4:vider la liste\n5:quiter le programme")
+          choix=input("   votre choix :") 
+          
           try:
-               print("-------------------------------------------------\nChoissez parmie ces 5 options suivantes :\n1:Ajouter un element à la liste\n2:Retirer un element de la liste\n3:Afficher la liste\n4:vider la liste\n5:quiter le programme")
-               choix=int(input("   votre choix :"))
-          except:
-               print("vous devez entrez u nombre") 
+               choix =int(choix)
+          except :
+               errmsg = "Erreur de saisie ! Vous devez entrer un nombre compris entre 1 et 5."
+               print(errmsg)
+                
           if choix==1:
                element= input("Donnez l'element à ajouter :")
                liste.append(element)
-               print("L'element à bien été ajouter à la liste")
+               print(f"{element}L'element à bien été ajouter à la liste")
           elif (choix==2):
-               element=input("donner le nom de l'element ")
+               element=input("donner le nom de l'element à retirer de la liste ")
+               
                if element in liste:
                     liste.remove(element)
-                    cont-=1
-                    print("l'élément a  bien été retirer de la liste!")
+                    
+                    print(f"{element} a  bien été retirer de la liste!")
+               if not (element  in liste):
+                    print(f"{element} l'est pas dans la liste !")
+                    
           elif (choix==3):
-               print("voici les informations que vous avez renseignez")
-               for i in liste:
-                    cont+=1
-                    print(f"{cont}.  {i}")
-               if not (i in liste):
+               print(" \nVoici vos informations dans le pannier :")
+               
+               for i ,element in enumerate(liste):
+                   
+                    print(f"{i}.  {element}")
+               if not (element  in liste):
                     print("votre liste ne contient aucun element")
-                    cont=0
+                    
           elif (choix==4):
                liste.clear()
                print("vous avez vider la liste")
           elif (choix==5):
                print("vous avez quitez le programme mercie")
                exit()
-               
-
+     if suivant=="non":
+          print("mercie au revoir !")
+          exit()
